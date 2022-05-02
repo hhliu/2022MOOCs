1a803f407a9685c5d63df023626b003c0bebd1af
hhliu <hunghuan.liu@gmail.com>
2022-05-02 17:58:28 +0800

    設定監聽的三種方式，方法三

--- app/src/main/java/cycu/nclab/proj/a2022moocs/MainActivity.java
+++ app/src/main/java/cycu/nclab/proj/a2022moocs/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     TextView tx;
     Button bt;
 
@@ -19,15 +19,31 @@ public class MainActivity extends AppCompatActivity {
 
         tx = findViewById(R.id.textView);
         bt = findViewById(R.id.button);
-        bt.setOnClickListener(myClickListener);
+        bt.setOnClickListener(this);
+        tx.setOnClickListener(this);
     }
 
-    private View.OnClickListener myClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            tx.setTextColor(Color.GREEN);
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.textView:
+                tx.setTextColor(getResources().getColor(R.color.purple_200));
+                tx.setText("Hello World!");
+                break;
+            case R.id.button:
+                tx.setTextColor(Color.GREEN);
+                tx.setText("點我、點我");
+                break;
         }
-    };
+    }
+
+// 方法二
+//    private View.OnClickListener myClickListener = new View.OnClickListener() {
+//        @Override
+//        public void onClick(View v) {
+//            tx.setTextColor(Color.GREEN);
+//        }
+//    };
 
 // 方法一
 //    public void onButtonClick(View view) {
