55b99e655e1c3bdf8dcdeef5367e59e7f221b31c
hhliu <hunghuan.liu@gmail.com>
2022-05-03 12:02:22 +0800

    ConstraintLayout

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-## 2022.05.02
-### **設置監聽器的三種方法**  
-#### 方法一：使用android:onClick屬性
+## 2022.05.03
+### **ConstraintLayout**  
 ---
 Note:  
-Android Studio: Bumblebee 2021.1.1 patch3, targetSDK: 31
-新版本Android Studio預設的Theme改為Theme.MaterialComponents，舊版本為Theme.AppCompt，這樣改動讓很多視圖元件的顯示和屬性設定變得不同，讓初學者在觀看舊專案的影片然後進行新專案的開發時產生一些困擾，尤其是本專案的minSDK設定為19的緣故，部分Material Design Component無法正常地被顯示。  
-修改方式為將minSDK設定為24或更高，想要挑戰自己的同學再維持minSDK為19
+在使用Button的時候，發現他的背景顏色無法被更改，這是由於新版本Android Studio預設使用了Theme.MaterialComponent所帶來的限制。詳細的說明需要去參考https://material.io/components/buttons的內容。一般處理的方式有三種，可任選一種使用：
+
+1. 在xml裡面，將Button元件改成使用androidx.appcompat.widget.AppCompatButton
+2. 在xml裡面，將app:backgroundTint屬性改成”@null”。(i.e., app:backgroundTint=”@null”)
+3. 將預設Theme改成使用parent="Theme.MaterialComponents.DayNight.NoActionBar.Bridge"
--- app/src/main/java/cycu/nclab/proj/a2022moocs/MainActivity.java
+++ app/src/main/java/cycu/nclab/proj/a2022moocs/MainActivity.java
@@ -1,53 +1,15 @@
 package cycu.nclab.proj.a2022moocs;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    TextView tx;
-    Button bt;
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        tx = findViewById(R.id.textView);
-        bt = findViewById(R.id.button);
-        bt.setOnClickListener(this);
-        tx.setOnClickListener(this);
+        setContentView(R.layout.activity_bookkeeping_constraintlayout);
     }
-
-    @Override
-    public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.textView:
-                tx.setTextColor(getResources().getColor(R.color.purple_200));
-                tx.setText("Hello World!");
-                break;
-            case R.id.button:
-                tx.setTextColor(Color.GREEN);
-                tx.setText("點我、點我");
-                break;
-        }
-    }
-
-// 方法二
-//    private View.OnClickListener myClickListener = new View.OnClickListener() {
-//        @Override
-//        public void onClick(View v) {
-//            tx.setTextColor(Color.GREEN);
-//        }
-//    };
-
-// 方法一
-//    public void onButtonClick(View view) {
-//        tx.setTextColor(Color.GREEN);
-//    }
-
 }
